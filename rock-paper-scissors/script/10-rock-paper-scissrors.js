bf314e256c93b0c9a4676f2906fdd75799a4166b
Lakshya_Pal <pallakshya077@gmail.com>
2025-07-27 01:52:50 +0530

    Fixed playerMove and computerMove

--- rock-paper-scissors/script/10-rock-paper-scissrors.js
+++ rock-paper-scissors/script/10-rock-paper-scissrors.js
@@ -28,38 +28,38 @@ function playGame(playerMove){
 
     let result;
 
-    if(playerMove === 'Scissors'){
-        if (computerMove === 'Scissors'){
+    if(playerMove === 'scissors'){
+        if (computerMove === 'scissors'){
       result = 'Tie'
       }
-    else if (computerMove === 'Rock'){
+    else if (computerMove === 'rock'){
       result = 'You Lose'
       }
-    else if (computerMove === 'Paper'){
+    else if (computerMove === 'paper'){
       result = 'You Won' 
       }
     }
 
-    else if(playerMove === 'Paper') {
-        if (computerMove === 'Paper'){
+    else if(playerMove === 'paper') {
+        if (computerMove === 'paper'){
     result = 'Tie'
     }
-  else if (computerMove === 'Scissors'){
+  else if (computerMove === 'scissors'){
     result = 'You Lose'
     }
-  else if (computerMove === 'Rock'){
+  else if (computerMove === 'rock'){
     result = 'You Won'
     }
     }
     
-    else if (playerMove === 'Rock'){
-        if (computerMove === 'Rock'){
+    else if (playerMove === 'rock'){
+        if (computerMove === 'rock'){
       result = 'Tie'
     }
-    else if (computerMove === 'Paper'){
+    else if (computerMove === 'paper'){
       result = 'You Lose'
     }
-    else if (computerMove === 'Scissors'){
+    else if (computerMove === 'scissors'){
       result = 'You Won'
     }
     }
@@ -78,8 +78,8 @@ function playGame(playerMove){
       innerHTML = `${result}.`;
     document.querySelector('.js-moves').
       innerHTML = `You
-      <img src="./images/${playerMove}-emoji.png" class="move-icon">
-      <img src="./images/${computerMove}-emoji.png" class="move-icon">
+      <img src="images/${playerMove}-emoji.png" class="move-icon">
+      <img src="images/${computerMove}-emoji.png" class="move-icon">
       Computer`;
 
       updateScoreElement();
@@ -96,11 +96,11 @@ function pickComputerMove(){
   const randomNumber = Math.random();
   let computerMove = '';
   if(randomNumber >= 0 && randomNumber <1/3){
-  computerMove = 'Rock';
+  computerMove = 'rock';
   } else if (randomNumber >=1/3 && randomNumber <= 2/3){
-  computerMove = 'Paper';
+  computerMove = 'paper';
   }else if(randomNumber >= 2/3 && randomNumber < 1){
-  computerMove = 'Scissors';
+  computerMove = 'scissors';
   }
   return computerMove;//The code ended here successfully 
   console.log('hello')// This code will not run.
