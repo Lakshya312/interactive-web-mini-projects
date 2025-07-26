e314d81060da3d5e56b738ef13794d3b5efbd41b
Lakshya_Pal <pallakshya077@gmail.com>
2025-07-27 01:36:02 +0530

    Fixed images path

--- rock-paper-scissors/script/10-rock-paper-scissrors.js
+++ rock-paper-scissors/script/10-rock-paper-scissrors.js
@@ -75,14 +75,14 @@ function playGame(playerMove){
     localStorage.setItem('score',JSON.stringify(score));
 
     document.querySelector('.js-result').
-  innerHTML = `${result}.`;
-document.querySelector('.js-moves').
-  innerHTML = `You
-  <img src="images/${playerMove}-emoji.png" class="move-icon">
-  <img src="images/${computerMove}-emoji.png" class="move-icon">
-  Computer`;
+      innerHTML = `${result}.`;
+    document.querySelector('.js-moves').
+      innerHTML = `You
+      <img src="./images/${playerMove}-emoji.png" class="move-icon">
+      <img src="./images/${computerMove}-emoji.png" class="move-icon">
+      Computer`;
 
-    updateScoreElement();
+      updateScoreElement();
   }
 
 function updateScoreElement(){
